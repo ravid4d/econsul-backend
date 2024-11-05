21cf5c8187c6c3cf26cba5cc51b0c4b0ca8ba435
ravid4d <ravichaudharyd4d@gmail.com>
2024-11-05 15:18:59 +0530

    add 2 days in event date of november

--- app/Http/Controllers/User/EventMessageController.php
+++ app/Http/Controllers/User/EventMessageController.php
@@ -22,7 +22,7 @@ class EventMessageController extends Controller
             ],
             [
                 'message' => "DV-$nextYear Deadline:",
-                'date' => Carbon::create($currentYear, 11, 5),
+                'date' => Carbon::create($currentYear, 11, 7),
             ],
             [
                 'message' => "DV-$nextYear Results:",
